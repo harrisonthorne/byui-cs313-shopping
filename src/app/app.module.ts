d0f69ec53b325bd40473eb70c41670a90acc4f45
Harrison Thorne <harrison.thorne@gmail.com>
2019-10-03 19:03:36 -0600

    add pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowseItemsComponent } from './browse-items/browse-items.component';
+import { ViewCartComponent } from './view-cart/view-cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ConfirmationPageComponent } from './confirmation-page/confirmation-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrowseItemsComponent,
+    ViewCartComponent,
+    CheckoutComponent,
+    ConfirmationPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/view-cart/view-cart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-cart',
+  templateUrl: './view-cart.component.html',
+  styleUrls: ['./view-cart.component.styl']
+})
+export class ViewCartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
