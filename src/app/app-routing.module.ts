2928cf2d8350e32443d9a2803a810a1f5ff8af14
Harrison Thorne <harrison.thorne@gmail.com>
2019-10-03 19:05:54 -0600

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BrowseItemsComponent } from 'src/app/browse-items/browse-items.component';
+import { CheckoutComponent } from 'src/app/checkout/checkout.component';
+import { ConfirmationPageComponent } from 'src/app/confirmation-page/confirmation-page.component';
+import { ViewCartComponent } from 'src/app/view-cart/view-cart.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '/browse',
+        component: BrowseItemsComponent
+    },
+    {
+        path: '/checkout',
+        component: CheckoutComponent
+    },
+    {
+        path: '/confirm',
+        component: ConfirmationPageComponent
+    },
+    {
+        path: '/cart',
+        component: ViewCartComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
